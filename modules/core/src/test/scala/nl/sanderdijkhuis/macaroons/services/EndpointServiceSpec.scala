0bc721e825cbaf972b6edf4579dea36891aea127
Sander Dijkhuis <sander.dijkhuis@cleverbase.com>
2021-03-31 23:09:31 +0200

    test: EndPointService.maybeLocation

---
+++ modules/core/src/test/scala/nl/sanderdijkhuis/macaroons/services/EndpointServiceSpec.scala
@@ -0,0 +1,25 @@
+package nl.sanderdijkhuis.macaroons.services
+
+import cats.effect.IO
+import cats.implicits._
+import eu.timepit.refined.auto._
+import nl.sanderdijkhuis.macaroons.domain.macaroon.{
+  Identifier,
+  Location,
+  Predicate,
+  RootKey
+}
+import weaver._
+
+object EndpointServiceSpec extends SimpleIOSuite {
+
+  test("keeps the location") {
+    val location = Location("https://example.com/")
+    def protect(rootKey: RootKey, predicate: Predicate) =
+      Identifier.from("id").pure[IO]
+    for {
+      loc1 <- EndpointService.make(None)(protect).maybeLocation
+      loc2 <- EndpointService.make(Some(location))(protect).maybeLocation
+    } yield assert.all(loc1.isEmpty, loc2.contains(location))
+  }
+}
